a01d5b85b14644dd58097f0e32d86601841d5f96
0SNP0 <36269903+0SNP0@users.noreply.github.com>
2024-02-23 15:37:18 +0300

    [HW2]: Added disabling of scheduler

--- scrapper/src/main/java/edu/java/scrapper/scheduler/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/scrapper/scheduler/LinkUpdaterScheduler.java
@@ -1,11 +1,13 @@
 package edu.java.scrapper.scheduler;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(value = "app.scheduler.enable", havingValue = "true", matchIfMissing = true)
 @EnableScheduling
 @Log4j2
 public class LinkUpdaterScheduler {
