d131a5c3723a684a5e53c8b75976ee7ceff284a8
0SNP0 <36269903+0SNP0@users.noreply.github.com>
2024-02-28 23:51:20 +0300

    [HW2]: Requested changes

--- scrapper/src/main/java/edu/java/scrapper/client/GithubClient.java
+++ scrapper/src/main/java/edu/java/scrapper/client/GithubClient.java
@@ -3,8 +3,14 @@ package edu.java.scrapper.client;
 import edu.java.scrapper.entities.GithubResponse;
 
 public class GithubClient extends AbstractClient<GithubResponse> {
+    private static final String DEFAULT_URL = "https://api.github.com";
+
     public GithubClient(String apiUrl) {
         super(apiUrl, GithubResponse.class);
+    }
+
+    public GithubClient() {
+        this(DEFAULT_URL);
     }
 
     @Override
--- scrapper/src/main/java/edu/java/scrapper/client/StackoverflowClient.java
+++ scrapper/src/main/java/edu/java/scrapper/client/StackoverflowClient.java
@@ -3,8 +3,14 @@ package edu.java.scrapper.client;
 import edu.java.scrapper.entities.StackoverflowResponse;
 
 public class StackoverflowClient extends AbstractClient<StackoverflowResponse> {
+    private static final String DEFAULT_URL = "https://api.stackexchange.com/2.3";
+
     public StackoverflowClient(String apiUrl) {
         super(apiUrl, StackoverflowResponse.class);
+    }
+
+    public StackoverflowClient() {
+        this(DEFAULT_URL);
     }
 
     @Override
